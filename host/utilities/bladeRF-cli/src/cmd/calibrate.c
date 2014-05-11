8af41b78c3aa2a85b6ac7f34875f5da257442cdf
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-05-11 19:38:53 -0400

    bladeRF-cli: Workaround - Added retry loop to TXLPF calibration
    The TXLPF calibration appears to fail on the first two attempts to
    run it. As a temporary workaround, a retry loop has been added
    around attempts to run this calibration.

--- host/utilities/bladeRF-cli/src/cmd/calibrate.c
+++ host/utilities/bladeRF-cli/src/cmd/calibrate.c
@@ -24,6 +24,38 @@
 #include "common.h"
 #include "cmd.h"
 
+/* The TXLPF calibration appears to fail the first couple times it is run.
+ * Until this is resolved, this retry loop is at least better than forcing
+ * the user to manually perform the retries. */
+static int txlpf_workaround(struct bladerf *dev)
+{
+    int status, cal_status;
+    unsigned int i;
+    const unsigned int max_retries = 5;
+
+    cal_status = BLADERF_ERR_UNEXPECTED;
+    for (i = 0; cal_status != 0 && i < max_retries; i++) {
+        status = bladerf_enable_module(dev, BLADERF_MODULE_TX, true);
+        if (status != 0) {
+            return status;
+        }
+
+        if (i != 0) {
+            printf("Retrying TXLPF calibration...\n");
+        }
+
+        /* Calibrate TX LPF Filter */
+        cal_status = bladerf_calibrate_dc(dev, BLADERF_DC_CAL_TX_LPF);
+
+        status = bladerf_enable_module(dev, BLADERF_MODULE_TX, false);
+        if (status != 0) {
+            return status;
+        }
+    }
+
+    return cal_status == 0 ? status : cal_status;
+}
+
 int cmd_calibrate(struct cli_state *state, int argc, char **argv)
 {
     /* Valid commands:
@@ -54,19 +86,8 @@ int cmd_calibrate(struct cli_state *state, int argc, char **argv)
             goto cmd_calibrate_err;
         }
 
-        /* Ensure both TX and RX are enabled */
-        status = bladerf_enable_module(state->dev, BLADERF_MODULE_TX, true);
-        if (status != 0) {
-            goto cmd_calibrate_err;
-        }
-
+        /* Enable the RX module */
         status = bladerf_enable_module(state->dev, BLADERF_MODULE_RX, true);
-        if (status != 0) {
-            goto cmd_calibrate_err;
-        }
-
-        /* Calibrate TX LPF Filter */
-        status = bladerf_calibrate_dc(state->dev, BLADERF_DC_CAL_TX_LPF);
         if (status != 0) {
             goto cmd_calibrate_err;
         }
@@ -77,14 +98,19 @@ int cmd_calibrate(struct cli_state *state, int argc, char **argv)
             goto cmd_calibrate_err;
         }
 
+        /* Calibrate TX LPF Filter */
+        status = txlpf_workaround(state->dev);
+        if (status != 0) {
+            goto cmd_calibrate_err;
+        }
+
         /* Calibrate RX VGA2 */
         status = bladerf_calibrate_dc(state->dev, BLADERF_DC_CAL_RXVGA2);
         if (status != 0) {
             goto cmd_calibrate_err;
         }
 
-        /* Disable TX and RX modules */
-        status = bladerf_enable_module(state->dev, BLADERF_MODULE_TX, false);
+        /* Disable RX module */
         status = bladerf_enable_module(state->dev, BLADERF_MODULE_RX, false);
 
     } else if (argc == 2) {
@@ -94,8 +120,8 @@ int cmd_calibrate(struct cli_state *state, int argc, char **argv)
         if (strcasecmp(argv[1], "tuning") == 0) {
             module = BLADERF_DC_CAL_LPF_TUNING;
         } else if (strcasecmp(argv[1], "txlpf") == 0) {
-            module = BLADERF_DC_CAL_TX_LPF;
-            status = bladerf_enable_module(state->dev, BLADERF_MODULE_TX, true);
+            status = txlpf_workaround(state->dev);
+            goto cmd_calibrate_err;
         } else if (strcasecmp(argv[1], "rxlpf") == 0) {
             module = BLADERF_DC_CAL_RX_LPF;
             status = bladerf_enable_module(state->dev, BLADERF_MODULE_RX, true);
@@ -111,11 +137,7 @@ int cmd_calibrate(struct cli_state *state, int argc, char **argv)
         status = bladerf_calibrate_dc(state->dev, module);
 
         if (module != BLADERF_DC_CAL_LPF_TUNING) {
-            if (module == BLADERF_DC_CAL_TX_LPF) {
-                status = bladerf_enable_module(state->dev, BLADERF_MODULE_TX, false);
-            } else {
-                status = bladerf_enable_module(state->dev, BLADERF_MODULE_RX, false);
-            }
+            status = bladerf_enable_module(state->dev, BLADERF_MODULE_RX, false);
         }
 
     } else {
